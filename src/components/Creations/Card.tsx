1f8cdad1b4db150913fa8329acbc09be8f72c16e
Supriya Mahato <codewithapu@gmail.com>
2024-01-28 20:46:59 +0530

    added footer

--- src/components/Creations/Card.tsx
+++ src/components/Creations/Card.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Creation } from '@/types/types';
 import styles from '@/styles/components/Creations/CreationCard.module.scss'
+import { motion } from 'framer-motion';
 
 interface CardProps {
     card: Creation;
 }
 
 const CreationCard: React.FC<CardProps> = ({ card }) => (
-    <div className={styles.CreationCard} key={card.id}>
+    <motion.div
+        initial={{ opacity: 0, y: 50 }} // Initial state before animation
+        animate={{ opacity: 1, y: 0 }} // Animate to this state
+        exit={{ opacity: 0, y: -50 }} // Exit animation state
+        transition={{ duration: 0.5 }}
+        className={styles.CreationCard}
+        key={card.id}
+    >
         <img draggable="false" className={styles.Thumbnail} src={card.thumbnail_url} alt={card.title} />
         <div className={styles.CardData}>
             <p className={styles.Tag}>
@@ -19,7 +27,7 @@ const CreationCard: React.FC<CardProps> = ({ card }) => (
             </h2>
             <p className={styles.Description}>{card.description}</p>
         </div>
-    </div>
+    </motion.div>
 );
 
 export default CreationCard;
