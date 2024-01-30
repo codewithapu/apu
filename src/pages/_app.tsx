4bb5524c01720c0b3022e92890bdb07fa2ca836d
Supriya Mahato <codewithapu@gmail.com>
2024-01-31 02:30:37 +0530

    fixed gfont error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,9 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Space+Grotesk:wght@300..700&family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
       <NavBar />
       <Component {...pageProps} />
     </>);
