cced4f545a396e6d806a33c7f12d22f76d3a20fc
Supriya Mahato <codewithapu@gmail.com>
2024-01-31 23:18:43 +0530

    added new creation

--- src/pages/creations/[slug].tsx
+++ src/pages/creations/[slug].tsx
@@ -78,6 +78,15 @@ const CreationPage: React.FC<CreationPageProps> = ({ creation }) => {
         >
             <Head>
                 <title>Creations</title>
+                <meta name="description" content="Design Engineer From India" />
+                <meta property="og:site_name" content="Supriyo M." />
+                <meta property="og:title" content={creation.title} />
+                <meta property="og:image" content={creation.thumbnail_url} />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+                <meta property="og:description"
+                    content={creation.description}
+                />
             </Head>
 
             <div className={styles.Wraper} data-lenis-prevent-touch>
